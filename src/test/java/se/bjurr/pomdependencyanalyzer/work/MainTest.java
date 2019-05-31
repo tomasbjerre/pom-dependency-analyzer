aef37c0f45755878d2980182095c1ed4c7290b13
Tomas Bjerre <tomas.bjerre85@gmail.com>
2019-05-31 09:46:21 +0200

    doc

--- src/test/java/se/bjurr/pomdependencyanalyzer/work/MainTest.java
+++ src/test/java/se/bjurr/pomdependencyanalyzer/work/MainTest.java
@@ -1,14 +1,23 @@
 package se.bjurr.pomdependencyanalyzer.work;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import se.bjurr.pomdependencyanalyzer.data.Dependency;
 import se.bjurr.pomdependencyanalyzer.main.Main;
 
 public class MainTest {
@@ -64,5 +73,29 @@ public class MainTest {
       "jobs/folder/pipeline" //
     };
     Main.main(args);
+  }
+
+  @Test
+  public void testRunWithMain2() throws Exception {
+    final URL resource = MainTest.class.getResource("/violation-comments-lib-1.87.pom.dot");
+    final String dotFile = resource.getFile();
+
+    final String[] args = {
+      "-sf",
+      tempDirWithPrefix.toFile().getAbsolutePath(),
+      "-d",
+      dotFile, //
+      "-ci",
+      "true" //
+    };
+    Main.main(args);
+    final String dependenciesFile =
+        tempDirWithPrefix.toFile().getAbsolutePath()
+            + "/se/bjurr/violations/violation-comments-lib/1.87/dependencies.json";
+    final byte[] bytes = Files.readAllBytes(Paths.get(dependenciesFile));
+    final String content = new String(bytes, StandardCharsets.UTF_8);
+    final List<Dependency> list =
+        new Gson().fromJson(content, new TypeToken<ArrayList<Dependency>>() {}.getType());
+    assertThat(list).hasSize(3);
   }
 }
